1139a6fe3104872fbafd42648fad61f144a8b42e
LeBaHien <hien.leba.775@gmail.com>
2025-08-17 07:31:14 +0700

    Adding function.c

---
+++ function.c
@@ -0,0 +1,33 @@
+/**
+ * @file       cbuffer.c
+ * @copyright
+ * @license
+ * @version    1.0.0
+ * @date
+ * @author     Group 5
+ * @brief      Circular Buffer
+ *             This Circular Buffer is safe to use in IRQ with single reader,
+ *             single writer. No need to disable any IRQ.
+ *
+ *             Capacity = <size> - 1
+ * @note       None
+ */
+/* Includes ----------------------------------------------------------- */
+#include "cbuffer.h"
+
+/* Public macros ------------------------------------------------------ */
+#define NOT_VALID                   0
+
+/* Public function ---------------------------------------------------- */
+void cb_init(cbuffer_t *cb, void *buf, uint32_t size)
+{
+    if (cb == NULL || buf == NULL)
+        return;
+    cb->data = (uint8_t*)buf;
+    cb->size = size;
+    cb->writer = 0;
+    cb->reader = 0;
+    cb->overflow = 0;
+    cb->active = true;
+    return;
+}
