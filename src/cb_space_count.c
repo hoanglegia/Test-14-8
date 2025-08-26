77129781229f9880292eddaec8a63c365c980b4a
baogiahoangle <hoanglegia.work@gmail.com>
2025-08-26 21:55:38 +0700

    update: put cbuffer.c into src folder

--- src/cb_space_count.c
+++
@@ -1,3 +0,0 @@
-uint32_t cb_space_count(cbuffer_t *cb){
-    return cb->size - cb_data_count(cb) - 1;
-}
--- src/cbuffer.c
+++ src/cbuffer.c
@@ -1,94 +1,79 @@
-/**
- * @file       cbuffer.c
- * @copyright
- * @license
- * @version    1.0.0
- * @date
- * @author     Group 5
- * @brief      Circular Buffer
- *             This Circular Buffer is safe to use in IRQ with single reader,
- *             single writer. No need to disable any IRQ.
- *
- *             Capacity = <size> - 1
- * @note       None
- */
-/* Includes ----------------------------------------------------------- */
 #include "cbuffer.h"
 
-/* Public function ---------------------------------------------------- */
-void cb_init(cbuffer_t *cb, void *buf, uint32_t size)
-{
-    if (cb == NULL || buf == NULL)
-        return;
-    cb->data = (uint8_t*)buf;
+void cb_init(cbuffer_t *cb, void *buf, uint32_t size){
+    cb->data = (uint8_t *)buf;
     cb->size = size;
     cb->writer = 0;
     cb->reader = 0;
     cb->overflow = 0;
     cb->active = true;
-    return;
 }
 
-void cb_clear(cbuffer_t *cb)
-{
-    if (cb == NULL)
-        return;
+void cb_clear(cbuffer_t *cb){
     cb->writer = 0;
     cb->reader = 0;
     cb->overflow = 0;
-    return;
+}
+
+uint32_t cb_data_count(cbuffer_t *cb){
+    if(cb->reader <= cb->writer){
+        return cb->writer - cb->reader;
+    }else{
+        return cb->writer + cb->size - cb->reader;
+    }
+}
+
+uint32_t cb_space_count(cbuffer_t *cb){
+    return cb->size - cb_data_count(cb) - 1;
+}
+
+uint32_t min(uint32_t a, uint32_t b){
+    if(a < b){
+        return a;
+    } else{
+        return b;
+    }
 }
 
 uint32_t cb_read(cbuffer_t *cb, void *buf, uint32_t nbytes)
 {
-    while (cb->reader != nbytes)
-    {
-        if ((cb->writer - cb->reader) == READER_BEFORE_WRITER_IS_FULL ||
-            (cb->writer + cb->size - cb->reader == WRITER_BEFORE_READER_IS_FULL))
-            return cb->reader;
-        else
-        {
-            if (buf != NULL)
-            {
-                ((uint8_t*)buf)[cb->reader] = cb->data[cb->reader];
-                cb->reader = (cb->reader + 1) % cb->size;
-            }
-        }    
+    if (cb == NULL || buf == NULL || !cb->active) {
+        return 0;
     }
-    return cb->reader;
+    uint32_t available = cb_data_count(cb);
+    if (available == 0) {
+        return 0;
+    }
+    uint32_t to_read = min(nbytes, available);
+    uint8_t *pbuf = (uint8_t *)buf;
+
+    for (uint32_t i = 0; i < to_read; i++) {
+        pbuf[i] = cb->data[cb->reader];
+        cb->reader = (cb->reader + 1) % cb->size;
+    }
+    return to_read;
 }
 
 uint32_t cb_write(cbuffer_t *cb, void *buf, uint32_t nbytes)
 {
-    while (cb->writer != nbytes)
-    {
-        if (cb_space_count(cb) == NO_SPACE_AVAILABLE)
-            return cb->writer;
-        else
-        {
-            if (buf != NULL)
-            {
-                cb->data[cb->writer] = ((uint8_t*)buf)[cb->writer];
-                cb->writer = (cb->writer + 1) % cb->size;
-            }
-        }    
+    if (cb == NULL || buf == NULL || !cb->active) {
+        return 0;
     }
-    return cb->writer;
+    uint32_t space = cb_space_count(cb);
+    if (space == 0) {
+        cb->overflow += nbytes; // tất cả data mất
+        return 0;
+    }
+    uint32_t to_write = min(nbytes, space);
+    uint8_t *pbuf = (uint8_t *)buf;
+
+    for (uint32_t i = 0; i < to_write; i++) {
+        cb->data[cb->writer] = pbuf[i];
+        cb->writer = (cb->writer + 1) % cb->size;
+    }
+    if (to_write < nbytes) {
+        cb->overflow += (nbytes - to_write);
+    }
+    return to_write;
 }
 
-uint32_t cb_data_count(cbuffer_t *cb) 
-{
-    if (cb == NULL) 
-        return ERROR_CB_DATA_COUNT;
-    if (cb->reader <= cb->writer)
-        return cb->writer - cb->reader;
-    else
-        return cb->writer + cb->size - cb->reader;
-}
-
-uint32_t cb_space_count(cbuffer_t *cb) 
-{
-    if (cb == NULL)
-        return ERROR_CB_SPACE_COUNT;
-    return cb->size - cb_data_count(cb) - 1;
-}
